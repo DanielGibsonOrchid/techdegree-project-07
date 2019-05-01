c0c65145b785ab7c833a16c00b9b5c123aadc07e
Daniel Gibson <danielgibson@Daniels-MacBook-Air.local>
2019-05-02 09:20:50 +1200

    Components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import './index.css';
+import { Container, Jumbotron } from 'react-bootstrap';
+import SearchForm from './components/Header.js';
 
 function App() {
   return (
     <div>
-      
+      <Jumbotron>
+        <Container>
+          <h1>React Gallery App - Project 7</h1>
+          <p>This is a gallery search app</p>
+
+          <SearchForm />
+          <br/>
+          <h2>Results</h2>
+        </Container>
+      </Jumbotron>
     </div>
   );
 }
