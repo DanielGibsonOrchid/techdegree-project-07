006aabd9ac0f52cebe9be5b64827e4aa65abc1c9
Daniel Gibson <danielgibson@Daniels-MacBook-Air.local>
2019-05-06 12:15:26 +1200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 //Simple header text
-const Header = ( {title} ) => (
+const Header = ( {title, subTitle} ) => (
     <header>
 
         <h1>{ title }</h1>
+        <p>{ subTitle }</p>
         <br />
 
     </header>
@@ -12,7 +13,8 @@ const Header = ( {title} ) => (
 
 //Edit heading text here 
 Header.defaultProps = {
-    title: 'React Gallery App - Project 7'
+    title: 'React Gallery App - Project 7',
+    subTitle: 'Search for images on Flickr or use to menu below'
 }
 
 export default Header;
